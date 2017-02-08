f1f9d2a1f4d18be05a68c7463d2965fcf4cb0d65
moikeha <kalanitechnology@gmail.com>
2017-02-08 06:41:46 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
  #!/bin/bash         
-set -e
-
 /bin/echo -e "\e[96m
 Updating...
 \e[0m"
@@ -16,6 +14,8 @@ Adding repos...
 sudo add-apt-repository ppa:philip.scott/elementary-tweaks
 sudo add-apt-repository ppa:numix/ppa
 sudo apt-add-repository ppa:george-edison55/nitroshare
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 /bin/echo -e "\e[96m
 Updating...
 \e[0m"
@@ -30,9 +30,13 @@ sudo apt-get install nitroshare
 sudo apt-get install gksu
 sudo apt-get install fail2ban
 sudo apt-get install gparted
-sudo apt-get install chromium-browser
 sudo apt-get install openssh-client
 sudo apt-get install openssh-server
+sudo apt-get install menulibre
+sudo apt-get install tlp
+sudo apt-get install google-chrome-stable
+sudo apt-get install libreoffice
+sudo apt-get install spotify-client
 /bin/echo -e "\e[96m
 Cleaning up...
 \e[0m"
